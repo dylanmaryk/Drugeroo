6c58920a8f5a393bb569b9b389103a8af0fcac0d
Dylan Maryk <dylan@door2door.io>
2017-06-10 15:52:53 +0200

    Add tab bar.

--- Drugeroo/ViewController.swift
+++ Drugeroo/ViewController.swift
@@ -6,20 +6,35 @@
 //  Copyright © 2017 drugeroos. All rights reserved.
 //
 
+import PusherSwift
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PusherDelegate {
+    @IBOutlet var tabBar: UITabBar!
+    
+    var pusher: Pusher!
+    var channel: PusherChannel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pusher = Pusher(key: "b91115f15fb79204ed0d",
+                        options: PusherClientOptions(authMethod: .endpoint(authEndpoint: "https://secure-temple-86252.herokuapp.com/pusher/auth"),
+                                                     host: .cluster("eu")))
+        pusher.delegate = self
+        pusher.connect()
+        
+        channel = pusher?.subscribe("private-my-channel")
+        
+        tabBar.selectedItem = tabBar.items?.first
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func subscribedToChannel(name: String) {
+        channel.trigger(eventName: "client-my-event", data: ["key" : "value"])
     }
-
-
+    
+    func debugLog(message: String) {
+        print(message)
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+target 'Drugeroo'
+pod 'PusherSwift'
+pod 'RxCocoa'
+pod 'RxSwift'
